ad19b30451fb737a53fc06d351668312033961e2
lelefontadev <lelelfonta@gmail.com>
2019-05-17 13:37:57 +0200

    classe ServerAsta 17/05/19

--- ProgettoAsta/src/progettoasta/ServerAsta.java
+++ ProgettoAsta/src/progettoasta/ServerAsta.java
@@ -25,9 +25,9 @@ public class ServerAsta {
         
         try{
             // CREO LA CONNESSIONE AL DATABASE
-           Class.forName(DB_DRV);
-           connection=DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWD);
-           statement=connection.createStatement();
+            Class.forName(DB_DRV);
+            connection=DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWD);
+            statement=connection.createStatement();
            
             //select utenti
             PreparedStatement selectUtente = connection.prepareStatement("SELECT * FROM utente");
@@ -37,12 +37,17 @@ public class ServerAsta {
             PreparedStatement selectOggetti = connection.prepareStatement("SELECT * FROM oggetto");
             ResultSet oggetti=selectUtente.executeQuery();
             
-       //Parser che produce l'oggetto dell'albero DOM (Document Object Model) dal contenuto XML
+            //select registrazione
+            PreparedStatement selectRegi = connection.prepareStatement("SELECT * FROM registrazione");
+            ResultSet registrazioni=selectRegi.executeQuery();
+            
+        //Parser che produce l'oggetto dell'albero DOM (Document Object Model) dal contenuto XML
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
          
         //Definisco un oggetto di tipo DocumentBuilder che fa da API per ottenere l'istanza del DOM
         DocumentBuilder builder = factory.newDocumentBuilder();
         
+        //CREA STRINGA CON XML
         
             ServerSocket socket = new ServerSocket(port);
             
@@ -54,8 +59,8 @@ public class ServerAsta {
                 Socket client = socket.accept();
                 
 
-                ThreadClient newConnect = new ThreadClient (client, treni, doc);
-                newConnect.start();
+               // ThreadClient newConnect = new ThreadClient (client, treni, doc);
+               // newConnect.start();
                 
             }
             
